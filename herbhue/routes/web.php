929adcd6b8a89a938603d947e9a5f965501def29
Ramyajeevan <146551917+Ramyajeevan@users.noreply.github.com>
2023-10-05 11:41:12 +0530

    Update web.php

--- herbhue/routes/web.php
+++ herbhue/routes/web.php
@@ -30,9 +30,9 @@ Artisan::Call('view:cache');
 Artisan::Call('route:cache');
 });
 
-Route::get('/login', function () {
+Route::get('login', function () {
     return view('login');
-});
+})->name('login');
 Route::get('/signup', function () {
     return view('signup');
 });
