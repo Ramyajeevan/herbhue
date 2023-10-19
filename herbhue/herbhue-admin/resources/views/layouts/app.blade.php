aa56629d18de9b11b0fa1b93a6e5be87c2db680b
Ramyajeevan <ramya@seodigitalindiamarketing.com>
2023-10-19 19:21:37 +0530

    commit now
    commit now

--- herbhue/herbhue-admin/resources/views/layouts/app.blade.php
+++ herbhue/herbhue-admin/resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
   @yield('title')
 	<!-- Favicon icon -->
 		<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.png') }}" />
