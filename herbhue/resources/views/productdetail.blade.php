fa22c176d9bfc2ef8665d4826a52697c8d32134e
Ramyajeevan <146551917+Ramyajeevan@users.noreply.github.com>
2023-10-20 18:14:39 +0530

    Update productdetail.blade.php

--- herbhue/resources/views/productdetail.blade.php
+++ herbhue/resources/views/productdetail.blade.php
@@ -272,7 +272,6 @@
     function addtocart()
     {
       var option_id = $("input[name='option_id']:checked").val();
-        alert(option_id);
       var quantity=$("#quantity").val();
       var url="{{URL('addtocart')}}";
       $.ajax(
