de157da9c9466cb6459f82797f6962a949cbf747
Ramyajeevan <146551917+Ramyajeevan@users.noreply.github.com>
2023-10-26 12:51:11 +0530

    Update header.blade.php

--- herbhue/herbhue-admin/resources/views/includes/header.blade.php
+++ herbhue/herbhue-admin/resources/views/includes/header.blade.php
@@ -85,7 +85,7 @@
             <li class="nav-item dropdown">
             <a
                 class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic"
-                href=""
+                href="javascript:void(0);"
                 data-bs-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false"
