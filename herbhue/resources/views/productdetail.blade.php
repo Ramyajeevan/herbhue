65126310974061226093ba9b6da8c7c547af46de
Rahul Kumar <146967576+Rahul-FrontendDev@users.noreply.github.com>
2023-10-29 20:56:41 +0530

    ui

--- herbhue/resources/views/productdetail.blade.php
+++ herbhue/resources/views/productdetail.blade.php
@@ -171,20 +171,20 @@
                     </div>
                     <div>
                         <h6>Benefits</h6>
-                        <div class="row mb-2">
+                        <div class="row ">
                             <div class="col-1">
                                 <img src="{{asset('img/check_circle_FILL0_wght400_GRAD0_opsz24.svg')}}"style="width:25px;" alt="">
                             </div>
                             <div class="col-11">
-                                <p>Nutrient profile: Lion's mane mushroom is rich in beta-glucans, which may support natural immunity and brain health</p>
+                                <p class="pb-1 mb-1">Nutrient profile: Lion's mane mushroom is rich in beta-glucans, which may support natural immunity and brain health</p>
                             </div>
                         </div>
-                        <div class="row mb-2">
+                        <div class="row">
                             <div class="col-1">
                                 <img src="{{asset('img/check_circle_FILL0_wght400_GRAD0_opsz24.svg')}}"style="width:25px;" alt="">
                             </div>
                             <div class="col-11">
-                                <p>Nutrient profile: Lion's mane mushroom is rich in beta-glucans, which may support natural immunity and brain health</p>
+                                <p class="pb-1 mb-1">Nutrient profile: Lion's mane mushroom is rich in beta-glucans, which may support natural immunity and brain health</p>
                             </div>
                         </div>
                     </div>
