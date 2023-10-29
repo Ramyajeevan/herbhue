ffec66482ba5d88f9d491cbfa70a7cca35f33e0f
Rahul Kumar <146967576+Rahul-FrontendDev@users.noreply.github.com>
2023-10-29 18:28:54 +0530

    ui

--- herbhue/resources/views/includes/header.blade.php
+++ herbhue/resources/views/includes/header.blade.php
@@ -36,7 +36,7 @@
         </div>
     </div>
 </div>
-<div class="container-fluid bg-green  pt-3">
+<div class="container-fluid bg-white  pt-3">
     <!-- <div class="container nav2">
         <div class="input-group mb-3 w-25 flex-nowrap">
             <span class="input-group-text bg-white border-0" id="basic-addon2"><img src="{{ asset('img/location.png') }}"
