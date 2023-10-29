24dfd024e3d0bb617c1414fd625eb2159fdd6ea3
Rahul Kumar <146967576+Rahul-FrontendDev@users.noreply.github.com>
2023-10-29 20:58:54 +0530

    ui

--- herbhue/resources/views/productdetail.blade.php
+++ herbhue/resources/views/productdetail.blade.php
@@ -252,31 +252,27 @@
                 <div class="container">
                         <h5 class="text-center text-black mb-4">You may like to purchase</h5>
                         <div class="cate-1 owl-carousel owl-theme">
-                            @foreach($product as $prod)
                             <div class="item">
                                 <div class="card border-secondary">
                                     <div class="card-body">
                                         <div class="text-center mb-3">
-                                            @if($prod->image1!="")
-                                            <img src="https://herbhue.azurewebsites.net/herbhue-admin/public/images/{{ $prod->image1 }}"style="width:100%; height:230px;" alt="">
-                                            @else
                                             <img src="{{ asset('img/no_image.svg') }}" style="width:100%; height:230px;" alt="">
-                                            @endif
+                                          
                                         </div>
                                         <p class="small text-center text-secondary py-0 my-0">220 gm</p>
                                         <h5 class="text-truncate text-center" style="max-width: 265px;">{{ $prod->name }}</h5>
                                         <p class="text-secondary text-center pb-1 mb-1  text-truncate" style="max-width: 265px;">
-                                        {!! $prod->description !!}</p>
-                                        <p class="text-secondary p-0 m-0 text-center">{{ $prod->describe }}</p>
+                                       </p>
+                                        <p class="text-secondary p-0 m-0 text-center"></p>
                                         <p class="text-secondary text-center pb-0 mb-0">M.R.P: <span
-                                                class="text-decoration-line-through text-secondary"> &pound; {{ $prod->product_options->mrp_price }}</span> </p>
-                                        <p class="text-center">&pound; {{ $prod->product_options->price }}</p>
+                                                class="text-decoration-line-through text-secondary"> &pound; </span> </p>
+                                        <p class="text-center">&pound; </p>
 
-                                        <a href="{{ route('productdetail', $prod->id) }}"  class="btn btn-secondary text-black fw-bold w-100">Add to cart</a>
+                                        <a href="#"  class="btn btn-secondary text-black fw-bold w-100">Add to cart</a>
                                     </div>
                                 </div>
                             </div>
-                            @endforeach
+                            
 
             </div>
                 </div>
