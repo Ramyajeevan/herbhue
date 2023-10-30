c8b9c9db4b3b2f180a0d1f8022ce2b8a8b2e55a5
Rahul Kumar <146967576+Rahul-FrontendDev@users.noreply.github.com>
2023-10-30 21:56:45 +0530

    ui

--- herbhue/resources/views/shop.blade.php
+++ herbhue/resources/views/shop.blade.php
@@ -4,8 +4,17 @@
 @endsection
 @section('content')
 <div class="container-fluid mb-5">
+    <img src="{{asset('img/shop-banner.png')}}" class="w-100" height="300px" alt="">
        <div class="container mt-5">
-       
+       <div class="row">
+        <div class="col-md-3">
+            <div class="card border-secondary shadow">
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+       </div>
         </div>
 </div>
 
