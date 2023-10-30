74b1cd6c0a9ed2f1f5d11e9e5e2acb03f0bd84be
Rahul Kumar <146967576+Rahul-FrontendDev@users.noreply.github.com>
2023-10-30 23:02:06 +0530

    ui

--- herbhue/resources/views/index.blade.php
+++ herbhue/resources/views/index.blade.php
@@ -235,10 +235,34 @@
 
 
     </div>
+    <div class="container-fluid">
+                <div class="container">
+                        <h5 class=" text-black mb-4">Trending Now</h5>
+                        <div class="cate-1 owl-carousel owl-theme">
+                            <div class="item">
+                            <div class="card border-secondary">
+
+                                    <div class="card-body">
+                                        <div class="text-center mb-3">
+                                        <img src="{{asset('img/medicine.png')}}" style="width:100%; height:230px;" alt="">
+                                        </div>
+                                        <p class="small text-center text-secondary py-0 my-0">220 gm</p>
+                                        <h5 class="text-truncate text-center" style="max-width: 265px;">Glucosamine HCL 1500 mg Tablet</h5>
+                                        <p class="text-secondary text-center pb-1 mb-1  text-truncate" style="max-width: 265px;">
+                                        Glucosamine HCL 1500 mg Tablet...</p>
+                                        
+                                        <a href="#" class="btn btn-outline-dark text-black bg-white fw-bold w-100 text-black">Buy <span class="text-decoration-line-through text-muted mx-1"> £ 260</span> <span class="text-black"> £ 260</span></a>
+                                    </div>
+
+                                    </div>
+                            </div>
+                            
+
+            </div>
+                </div>
 
 
 
-    
 
     
     <!-- <div class="container-fluid   py-5 ">
