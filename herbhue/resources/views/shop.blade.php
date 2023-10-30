625f2ede52c17e631dd6e857df3ec1da73ef798c
Rahul Kumar <146967576+Rahul-FrontendDev@users.noreply.github.com>
2023-10-30 21:52:42 +0530

    ui

---
+++ herbhue/resources/views/shop.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('title')
+<title>Herbhue Personalization Page</title>
+@endsection
+@section('content')
+<div class="container-fluid mb-5">
+       <div class="container mt-5">
+       
+        </div>
+</div>
+
+ 
+@endsection
