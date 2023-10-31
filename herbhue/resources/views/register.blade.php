0d90521539c025234810f35d80b0cd8437366c00
Rahul Kumar <146967576+Rahul-FrontendDev@users.noreply.github.com>
2023-10-31 23:10:20 +0530

    ui

--- herbhue/resources/views/register.blade.php
+++ herbhue/resources/views/register.blade.php
@@ -44,7 +44,7 @@
                     <div class="input-group mb-3">
                     <span class="input-group-text bg-white" id="basic-addon1"><img src="{{ asset('img/UK_flag.png') }}"
                    width="15px" alt=""> +44</span> 
- <input type="text" class="form-control py-2"  id="mobile" name="mobile" onkeypress="return onlyNumberKey(event);" maxlength = "10" aria-describedby="basic-addon1" required placeholder="Phone No.">
+                    <input type="text" class="form-control py-2"  id="mobile" name="mobile" onkeypress="return onlyNumberKey(event);" maxlength = "10" aria-describedby="basic-addon1" required placeholder="Phone No.">
                     </div>
                     <!-- <div class="form-group">
                         <input type="text"  id="mobile" name="mobile" onkeypress="return onlyNumberKey(event);" maxlength = "10"  required>
