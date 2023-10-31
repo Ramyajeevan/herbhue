759bb36e9ff6d25226d6915457596a224ade996a
Rahul Kumar <146967576+Rahul-FrontendDev@users.noreply.github.com>
2023-10-31 23:25:37 +0530

    ui

--- herbhue/resources/views/thankyou.blade.php
+++ herbhue/resources/views/thankyou.blade.php
@@ -6,16 +6,16 @@
 
 @endsection
 @section('content')
-<div class="container-fluid mb-5">
+<div class="container-fluid my-5">
     <div class="container">
         <div class="row">
-            <div class="col-md-8 border-end border-secondary">
+            <div class="col-md-8 border-end">
                 <div class="d-flex justify-content-center">
                     <img src="{{asset('img/order-successful.png')}}" style="width:150px" alt="">
                 </div>
 
                 <h3 class="text-black fw-bold text-center">Order Confirmed</h3>
-                <p class="fs-5 text-center ">Your order is currently being processed. You will received an order confirmation email shortly.</p>
+                <p class="fs-5 text-center ">Your order is currently being processed. <br> You will received an order confirmation email shortly.</p>
 
                 <p class="fs-5 text-center ">Total Price for item £482.00</p>
                 <p class="fs-5 text-center "><a href="#" class="text-decoration-none text-black"> View Details > </a></p>
@@ -24,7 +24,7 @@
                 </div>
               
             </div>
-            <div class="col-md-4  pt-4">
+            <div class="col-md-4  pt-5">
                 <p class="fs-4 text-muted fw-bold">Thank you for shopping with us. <br> Visit again !</p>
             </div>
         </div>
