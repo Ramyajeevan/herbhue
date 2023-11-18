e010fed078cba290d3a040960d29e87ffe449b03
Ramyajeevan <ramya@seodigitalindiamarketing.com>
2023-11-18 15:01:23 +0530

    Update index.blade.php

--- herbhue/herbhue-admin/resources/views/Backend/Newsletter/index.blade.php
+++ herbhue/herbhue-admin/resources/views/Backend/Newsletter/index.blade.php
@@ -114,10 +114,22 @@
   </div>
 </div>
 @endsection
+@if(count($emails)>0)
 @section('script')
     <!-- --------------------------------------------------------------- -->
     <!-- This page JavaScript -->
     <!-- --------------------------------------------------------------- -->
+    <script src="{{ asset('dist/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <!-- start - This is for export functionality only -->
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
+    <script src="{{ asset('dist/js/pages/datatable/datatable-advanced.init.js') }}"></script>
+
 <script>
   function deletenewsletter(id)
   {
